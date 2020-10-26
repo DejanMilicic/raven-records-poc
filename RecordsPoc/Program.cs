a0c518098314110909633ce2ffe1fa770a4e20de
Djordje Djukic <djukicdjole@gmail.com>
2020-10-26 23:35:45 +0100

    raven poc

---
+++ RecordsPoc/Program.cs
@@ -0,0 +1,28 @@
+﻿using Raven.Client.Documents;
+
+namespace RecordsPoc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var store = new DocumentStore
+            {                
+                Urls = new[] { "http://localhost:8080" },
+                Database = "test-records",
+            };
+
+            store.Initialize();
+
+            using (var session = store.OpenSession())
+            {
+                var person = new Person("John", "Doe");
+
+                session.Store(person);
+                session.SaveChanges();
+            }
+        }        
+    }
+
+    public record Person(string FirstName, string LastName);
+}
